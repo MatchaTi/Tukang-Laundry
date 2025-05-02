b0559dcca81da7feb9f28a91dca94369b33bd6eb
ifnuu01 <ifnuu01@gmail.com>
2025-05-02 20:08:38 +0800

    feat : spring boot

---
+++ Back-End/src/main/java/TukangLaundry/restful/TukanglaundryapiApplication.java
@@ -0,0 +1,13 @@
+package TukangLaundry.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukanglaundryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukanglaundryapiApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/TukangLaundry/restful/TukanglaundryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package TukangLaundry.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukanglaundryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
