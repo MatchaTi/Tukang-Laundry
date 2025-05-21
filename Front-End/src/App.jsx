a1a69864afedd69588a9dd9cb013f4dd35ffd064
MatchaTi <tehmatcha28@gmail.com>
2025-05-21 21:26:58 +0800

    chore: update routing

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,9 +1,39 @@
-import { useParams } from 'react-router';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Dashboard from './pages/Dashboard';
+import DetailPaket from './pages/DetailPaket';
+import DetailPesanan from './pages/DetailPesanan';
+import LandingPage from './pages/LandingPage';
+import ListKasir from './pages/ListKasir';
+import ListPaket from './pages/ListPaket';
+import ListPesanan from './pages/ListPesanan';
+import Login from './pages/Login';
+import TambahKasir from './pages/TambahKasir';
+import TambahPaket from './pages/TambahPaket';
+import TambahPesanan from './pages/TambahPesanan';
+import UbahKasir from './pages/UbahKasir';
+import UbahPaket from './pages/UbahPaket';
+import UbahPesanan from './pages/UbahPesanan';
 
 function App() {
-    let { id } = useParams();
-    return <div>{id}</div>;
+    return (
+        <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/list-pesanan' element={<ListPesanan />} />
+            <Route path='/list-pesanan/tambah-pesanan' element={<TambahPesanan />} />
+            <Route path='/list-pesanan/detail-pesanan/:id' element={<DetailPesanan />} />
+            <Route path='/list-pesanan/ubah-pesanan/:id' element={<UbahPesanan />} />
+            <Route path='/paket-layanan' element={<ListPaket />} />
+            <Route path='/paket-layanan/tambah-paket' element={<TambahPaket />} />
+            <Route path='/paket-layanan/detail-paket/:id' element={<DetailPaket />} />
+            <Route path='/paket-layanan/ubah-paket/:id' element={<UbahPaket />} />
+            <Route path='/list-kasir' element={<ListKasir />} />
+            <Route path='/list-kasir/tambah-kasir' element={<TambahKasir />} />
+            <Route path='/list-kasir/ubah-kasir/:id' element={<UbahKasir />} />
+        </Routes>
+    );
 }
 
 export default App;
--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,44 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
 import './index.css';
-import DetailPaket from './pages/DetailPaket.jsx';
-import DetailPesanan from './pages/DetailPesanan.jsx';
-import ListKasir from './pages/ListKasir.jsx';
-import ListPaket from './pages/ListPaket.jsx';
-import ListPesanan from './pages/ListPesanan.jsx';
-import Login from './pages/Login.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import Home from './pages/LandingPage.jsx';
-import TambahKasir from './pages/TambahKasir.jsx';
-import TambahPaket from './pages/TambahPaket.jsx';
-import TambahPesanan from './pages/TambahPesanan.jsx';
-import UbahKasir from './pages/UbahKasir.jsx';
-import UbahPaket from './pages/UbahPaket.jsx';
-import UbahPesanan from './pages/UbahPesanan.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/c/:id' element={<App />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='/home' element={<Home />} />
-                <Route path='/list-pesanan' element={<ListPesanan />} />
-                <Route path='/list-pesanan/tambah-pesanan' element={<TambahPesanan />} />
-                <Route path='/list-pesanan/detail-pesanan/:id' element={<DetailPesanan />} />
-                <Route path='/list-pesanan/ubah-pesanan/:id' element={<UbahPesanan />} />
-                <Route path='/paket-layanan' element={<ListPaket />} />
-                <Route path='/paket-layanan/tambah-paket' element={<TambahPaket />} />
-                <Route path='/paket-layanan/detail-paket/:id' element={<DetailPaket />} />
-                <Route path='/paket-layanan/ubah-paket/:id' element={<UbahPaket />} />
-                <Route path='/list-kasir' element={<ListKasir />} />
-                <Route path='/list-kasir/tambah-kasir' element={<TambahKasir />} />
-                <Route path='/list-kasir/ubah-kasir/:id' element={<UbahKasir />} />
-            </Routes>
+            <App />
         </BrowserRouter>
     </StrictMode>,
 );
