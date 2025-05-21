0336a8cb0e34be7c0c522935e143ddd78f063552
MatchaTi <tehmatcha28@gmail.com>
2025-05-21 21:03:44 +0800

    chore: update components

--- Front-End/src/components/Badge.jsx
+++ Front-End/src/components/Badge.jsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
-import React from 'react';
 
 export default function Badge({ state, children }) {
-    return <div className={clsx('badge w-full', state)}>{children}</div>;
+    return <div className={clsx('badge h-fit w-full', state)}>{children}</div>;
 }
